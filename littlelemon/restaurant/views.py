5ea44b131dec2eda6bd017897840bd13ab9bb46e
CesaHub00 <cesarenava247@gmail.com>
2024-04-26 13:38:17 +0200

    update auth

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 from django.contrib.auth.models import User
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer, UserSerializer
@@ -16,10 +18,14 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
+
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
+
 class UserViewSet(viewsets.ModelViewSet):
+   permission_classes = [IsAuthenticated]
    queryset = User.objects.all()
    serializer_class = UserSerializer
